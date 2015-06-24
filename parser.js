ecd9ed71e6efae215f4aebfe68de6797b00a646b
jschanker <jschanker@molloy.edu>
2015-06-24 16:09:36 -0400

    Added support for more blocks
    Now can handle if/else if/else blocks, remainder, not, less/greater than or equal to, parenthesized expressions.

--- parser.js
+++ parser.js
@@ -5,12 +5,15 @@
         tokens : {
             integer                :  [/-\d+|\d+/],
             variable               :  [/[A-Za-z_]\w*/],
+            word                   :  [/[A-Za-z]+/],
             equals                 :  ["is equal to", "equals to", "equals", "="],
             comma                  :  [","],
             semicolon              :  [";"],
             "if"                   :  ["if"],
+            "else"                 :  ["otherwise", "else"],
             set                    :  ["set"],
             then                   :  ["then"],
+            commaThen              :  [", then", ",then", ",", "then"],
             "while"                :  ["repeat while", "do while", "while", "keep going while"],
             until                  :  ["repeat until", "do until", "until", "keep going until"],
             "for"                  :  ["for each", "for every", "go through every", "go through each", 
@@ -22,17 +25,30 @@
             increase               :  ["increase", "augment", "increment"],
             decrease               :  ["decrease", "decrement"],
             by                     :  ["by"],
+            not                    :  ["not"],
             or                     :  ["or"],
             and                    :  ["and"],
             lessThan               :  ["is less than", "less than", "<"],
+            lessThanOrEq           :  ["is less than or equals", "is less than or equal to", "less than or equals", 
+                                       "less than or equal to", "<=", "is not greater than", "is not more than"],
             greaterThan            :  ["is greater than", "greater than", "is more than", "exceeds", "more than", ">"],
+            greaterThanOrEq        :  ["is greater than or equals", "is greater than or equal to", "greater than or equals", 
+                                       "greater than or equal to", ">=", "is not less than"],
+            notEquals              :  ["is not equal to", "does not equal", "not equal to"],
+            divisibleBy            :  ["is divisible by", "divisible by", "is a multiple of", "is multiple of"],
+            factorOf               :  ["is a factor of", "is factor of"],
             plus                   :  ["\\+", "plus", "add"],
             minus                  :  ["-", "minus", "subtract"],
             multiply               :  ["\\*", "multiply", "times"],
             divide                 :  ["\\/", "divided by", "divide"],
+            modulo                 :  ["modulo", "mod", "modulus"],
+            remainderOf            :  ["the remainder of", "remainder of"],
             power                  :  ["\\^", "raised to the", "to the", "power"],
+            quotationMark          :  ['\\"', "\\'"],
             openBracket            :  ["{"],
             closeBracket           :  ["}"],
+            openParenthesis        :  ["\\("],
+            closeParenthesis       :  ["\\)"],
             statementSeparator     :  [/\.|;|\r\n|\r|\n/],
             fullStatementSeparator :  [/\.\s*|\r\n|\r|\n/]
         },
@@ -42,10 +58,17 @@
             expressionTrue         :  ["true"],
             expressionFalse        :  ["false"],
             expressionLessThan     :  ["expressionNumerical", "lessThan", "expressionNumerical"],
+            expressionLessThanEq   :  ["expressionNumerical", "lessThanOrEq", "expressionNumerical"],
             expressionGreaterThan  :  ["expressionNumerical", "greaterThan", "expressionNumerical"],
+            expressionGreaterThanEq:  ["expressionNumerical", "greaterThanOrEq", "expressionNumerical"],
             expressionEquals       :  ["expressionNumerical", "equals", "expressionNumerical"],
+            expressionNotEquals    :  ["expressionNumerical", "notEquals", "expressionNumerical"],
+            expressionDivisibleBy  :  ["expressionNumerical", "divisibleBy", "expressionNumerical"],
+            expressionFactorOf     :  ["expressionNumerical", "factorOf", "expressionNumerical"],
             expressionOr           :  ["expressionLogical", "or", "expressionLogical"],
             expressionAnd          :  ["expressionLogical", "and", "expressionLogical"],
+            expressionNot          :  ["not", "expressionLogical"],
+            expressionLogicalBlock :  ["openParenthesis", "expressionLogical", "closeParenthesis"]
         },
 
         expressionNumerical : {
@@ -55,42 +78,67 @@
             expressionProduct      :  ["expressionNumerical", "multiply", "expressionNumerical"],
             expressionDivide       :  ["expressionNumerical", "divide", "expressionNumerical"],
             expressionPower        :  ["expressionNumerical", "power", "expressionNumerical"],
+            expressionRemainderOf  :  ["remainderOf", "expressionNumerical", "divide", "expressionNumerical"], // divide
+            expressionModulo       :  ["expressionNumerical", "modulo", "expressionNumerical"],
             expressionVariable     :  ["variable"],
+            expressionNumBlock     :  ["openParenthesis", "expressionNumerical", "closeParenthesis"]
+        },
+
+        expressionString : {
+            expressionText         :  ["quotationMark", "word", "quotationMark"]
         },
 
         expressionGeneral : {
             expressionGenNumerical :  ["expressionNumerical"],
             expressionGenLogical   :  ["expressionLogical"],
+            expressionGenString    :  ["expressionString"]
+        },
+
+        statementConditional : {
+            statementElseIf        :  ["else", "if", "expressionLogical", "commaThen", "statementGeneral"],
+            statementElseIfMore    :  ["else", "if", "expressionLogical", "commaThen", "statementGeneral", 
+                                       "semicolon", "statementConditional"],
+            statementElse          :  ["else", "statementGeneral"]
         },
 
         statementGeneral : {
             statementBlock         :  ["openBracket", "statementGeneral", "closeBracket"],
             statementJoin          :  ["statementGeneral", "semicolon", "statementGeneral"],
             statementAssignment    :  ["set", "variable", "to", "expressionGeneral"],
-            statementIncreaseVar   :  ["increase", "variable", "by", "expressionGeneral"],
-            statementDecreaseVar   :  ["decrease", "variable", "by", "expressionGeneral"],
+            statementIncreaseVar   :  ["increase", "variable", "by", "expressionNumerical"],
+            statementDecreaseVar   :  ["decrease", "variable", "by", "expressionNumerical"],
             statementForLessThan   :  ["for", "variable", "lessThan", "expressionNumerical", "comma", "statementGeneral"],
             statementForFromTo     :  ["for", "variable", "from", "expressionNumerical", "to", 
                                        "expressionNumerical", "comma", "statementGeneral"],
             statementWhile         :  ["while", "expressionLogical", "comma", "statementGeneral"],
             statementUntil         :  ["until", "expressionLogical", "comma", "statementGeneral"],
-            statementIfComma       :  ["if", "expressionLogical", "comma", "statementGeneral"],
-            statementIfThen        :  ["if", "expressionLogical", "then", "statementGeneral"]
+            //statementIfComma     :  ["if", "expressionLogical", "comma", "statementGeneral"],
+            statementIfThen        :  ["if", "expressionLogical", "commaThen", "statementGeneral"],
+            statementIfThenAlt     :  ["if", "expressionLogical", "commaThen", "statementGeneral", 
+                                       "semicolon", "statementConditional"]
         }
     };
 
     namespace.exports.statementPartSchema = statementPartSchema;
 
     var processingOrder = {
-        expressionGeneral   : ["expressionGenLogical", "expressionGenNumerical"],
-        expressionLogical   : ["expressionOr", "expressionAnd", "expressionLessThan", "expressionGreaterThan", 
-                               "expressionEquals", "expressionTrue", "expressionFalse"],
-        expressionNumerical : ["expressionSum", "expressionDifference", "expressionProduct", "expressionDivide", 
-                               "expressionPower", "expressionInteger", "expressionVariable"],
-        statementGeneral    : ["statementBlock", "statementJoin", "statementAssignment", "statementIncreaseVar", 
-                               "statementDecreaseVar", "statementForLessThan", "statementForFromTo", "statementWhile", 
-                               "statementUntil", "statementIfComma", "statementIfThen"]
+        expressionGeneral     : ["expressionGenLogical", "expressionGenNumerical", "expressionGenString"],
+        expressionLogical     : ["expressionOr", "expressionAnd", "expressionNot", 
+                                 "expressionNotEquals", "expressionLessThanEq", "expressionGreaterThanEq", 
+                                 "expressionLessThan", "expressionGreaterThan", 
+                                 "expressionEquals", "expressionDivisibleBy", "expressionFactorOf", 
+                                 "expressionLogicalBlock", "expressionTrue", "expressionFalse"],
+        expressionNumerical   : ["expressionSum", "expressionDifference", 
+                                 "expressionProduct", "expressionDivide", "expressionModulo", "expressionRemainderOf",
+                                 "expressionPower", "expressionVariable", "expressionNumBlock", "expressionInteger"],
+        expressionString      : ["expressionText"],
+        statementGeneral      : ["statementBlock", "statementJoin", "statementAssignment", "statementIncreaseVar", 
+                                 "statementDecreaseVar", "statementForLessThan", "statementForFromTo", "statementWhile", 
+                                 "statementUntil", "statementIfThenAlt", "statementIfThen"],
+        statementConditional  : ["statementElseIfMore", "statementElseIf", "statementElse"]
     };
+
+    namespace.exports.processingOrder = processingOrder;
 
     var getPartsArr = function(schemaName) {
         var schemaCategory;
@@ -182,7 +230,7 @@
 
         if(tokenIndex <= 0) {
             // For efficiency : don't need to check each possible match if token needs to be at start
-            parseResultsArr = parseSchemaFragment(textRemaining, partsArr);
+            parseResultsArr = parseSchemaFragment(textRemaining, partsArr, matchEntire);
         } else {
             splitOnTokenArr = splitOnFirst(textRemaining, tokenName);
 
@@ -232,7 +280,7 @@
     };
 
     namespace.exports.parseText = function(text) {
-        text = document.getElementById("pseudocode").value; // Remove later
+        //text = document.getElementById("pseudocode").value; // Remove later
         var statementCount = text.split(statementPartSchema.tokens.statementSeparator).length;
         var statements = text.split(statementPartSchema.tokens.fullStatementSeparator[0]);
         var parsedStatements = [];
